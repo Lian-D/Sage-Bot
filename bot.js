b9ec904298e99a87f4375b35a4ea54301cd51e03
Chris Wang <33915928+TheDragQueen@users.noreply.github.com>
2019-10-14 13:34:13 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,12 +18,7 @@ client.on('message', message => {
     if (message.content == "kaj"){
         message.channel.send(" ", { files: ["./resources/tenor.gif"] });
     }
-    if (message.content == "epic"){
-        message.channel.send("Le epic win", { files: ["./resources/based.gif"] });
-    }
-    if (message.content == "Fortnite"){
-        message.channel.send(" ", { files: ["./resources/27lrwk.jpg"] });
-    }
+
 
     if (message.content == "gondola"){
         var randomNumber2 = (Math.floor(Math.random() * 2) + 1);
