4d5c829604e6bd47c5ace8479a91acad533adefd
Lian <Lian.Duan@alumni.ubc.ca>
2019-10-16 19:48:15 -0700

    Chris literally cannot code

--- bot.js
+++ bot.js
@@ -12,30 +12,25 @@ client.on('message', message => {
     if (message.content == "dab"){
         message.channel.send(" ", { files: ["./resources/dab2.png"] });
     }
-    if (message.content == "our lord and savior"){
+    else if (message.content == "our lord and savior"){
         message.channel.send(" ", { files: ["./resources/lord.png"] });
     }
-    if (message.content == "kaj"){
+    else if (message.content == "kaj"){
         message.channel.send(" ", { files: ["./resources/tenor.gif"] });
     }
-    if (message.content == "epic"){
+    else if (message.content == "epic"){
         message.channel.send("Le epic win", { files: ["./resources/based.gif"] });
     }
-    
-        if (message.content == "epic"){
-        message.channel.send("Le epic win", { files: ["./resources/based.gif"] });
-    }
-    if (message.content == "Fortnite"){
+    else if (message.content == "Fortnite"){
         message.channel.send(" ", { files: ["./resources/27lrwk.jpg"] });
     }
-        if (message.content == "walter"){
+    else if (message.content == "walter"){
         message.channel.send("libtard destroyed", { files: ["./resources/libtard.png"] });
     }
-
-    if (message.content == "Fortnite"){
+    else if (message.content == "Fortnite"){
         message.channel.send(" ", { files: ["./resources/27lrwk.jpg"] });
     }
-    if (message.content == "gondola"){
+   else if (message.content == "gondola"){
         var randomNumber2 = (Math.floor(Math.random() * 2) + 1);
         if (randomNumber2 == 1){
             message.channel.send("Enjoy the ride https://gondola.stravers.net/random-raw", { files: ["./resources/gondola.webm"] });
@@ -44,7 +39,6 @@ client.on('message', message => {
             message.channel.send("Enjoy the ride https://gondola.stravers.net/random-raw", { files: ["./resources/gondola2.webm"] });
         }
     }
-    
     var response = responses.response(message.content);
     if (response != null){
         message.channel.send(response);
