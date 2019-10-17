7f62412fcf1ba53bd1c4a50427763e51421712db
Chris Wang <33915928+TheDragQueen@users.noreply.github.com>
2019-10-17 15:57:40 -0700

    HMHMHMHMMHHM

--- bot.js
+++ bot.js
@@ -32,12 +32,45 @@ client.on('message', message => {
     }
    else if (message.content == "gondola"){
         var randomNumber2 = (Math.floor(Math.random() * 2) + 1);
+       
         if (randomNumber2 == 1){
             message.channel.send("Enjoy the ride https://gondola.stravers.net/random-raw", { files: ["./resources/gondola.webm"] });
         }
         else {
             message.channel.send("Enjoy the ride https://gondola.stravers.net/random-raw", { files: ["./resources/gondola2.webm"] });
         }
+    }
+    else if (message.content == "minion"){
+        var randomNumber2 = (Math.floor(Math.random() * 9) + 1);
+        
+        if (randomNumber2 == 1){
+            message.channel.send("GOD I FUCKING LOVE MINIONS", { files: ["./BLARTMINION.png"] });
+        }
+        if (randomNumber2 == 2){
+            message.channel.send("GOD I FUCKING LOVE MINIONS", { files: ["./MINION LOVER1.5.png"] });
+        }
+        if (randomNumber2 == 3){
+            message.channel.send("GOD I FUCKING LOVE MINIONS", { files: ["./MINION LOVER2.5.png"] });
+        }        
+        if (randomNumber2 == 4){
+            message.channel.send("GOD I FUCKING LOVE MINIONS", { files: ["./MINION LOVER3.png"] });
+        }
+        if (randomNumber2 == 5){
+            message.channel.send("GOD I FUCKING LOVE MINIONS", { files: ["./MINION SNIPER.png"] });
+        }
+        if (randomNumber2 == 6){
+            message.channel.send("GOD I FUCKING LOVE MINIONS", { files: ["./RED MINION.png"] });
+        }
+        if (randomNumber2 == 7){
+            message.channel.send("GOD I FUCKING LOVE MINIONS", { files: ["./SCHINDLERS MINION.png"] });
+        }
+        if (randomNumber2 == 8){
+            message.channel.send("GOD I FUCKING LOVE MINIONS", { files: ["./minion_guitar.png"] });
+        }
+        if (randomNumber2 == 9){
+            message.channel.send("GOD I FUCKING LOVE MINIONS", { files: ["./whydoeshewearthemask.png"] });
+        }        
+        
     }
     var response = responses.response(message.content);
     if (response != null){
