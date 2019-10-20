a05949d6a9952dbb40d9bbee5bf8464de1d6e7d5
Lian <Lian.Duan@alumni.ubc.ca>
2019-10-19 21:25:33 -0700

    idk how to get wikipedia function to work so i'll scrap that for now

--- core/wikipedia/wikipedia.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    keywords: ["who is ", "what is "],
-    response: function(message, search) {
-        message.channel.send('looking up in wikipedia');
-        var wiki = require('wikijs');
-        var response = wiki().page(search.trim()).then(page => page.summary());
-        message.channel.send(response);
-    }
-};
