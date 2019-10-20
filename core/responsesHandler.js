31c7313232c9d87a7356b84bdaa552d36efcfae4
Lian <Lian.Duan@alumni.ubc.ca>
2019-10-19 18:09:35 -0700

    making progress

--- core/responsesHandler.js
+++ core/responsesHandler.js
@@ -1,9 +1,9 @@
 module.exports = {
     response: function(bot, message){
         //Responses
-        var garfResponse = require('./core/commands/garf.js');
-        var imageResponse = require('./core/commands/imageResponse.js');
-        var stringResponse = require('./core/commands/stringResponse.js');
+        var garfResponse = require('./commands/garf.js');
+        var imageResponse = require('./commands/imageResponse.js');
+        var stringResponse = require('./commands/stringResponse.js');
 
         message.channel.send("hi2");
     }
