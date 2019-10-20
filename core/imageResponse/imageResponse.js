49f379234b768639e428a841e0bd4b302e02edb3
Lian <Lian.Duan@alumni.ubc.ca>
2019-10-19 19:14:49 -0700

    testing again

--- core/imageResponse/imageResponse.js
+++ core/imageResponse/imageResponse.js
@@ -6,22 +6,22 @@
 module.exports = {
     keywords: ["dab", "kaj", "epic", "our lord and savior", "minion"],
     response: function(message){
-        // message.channel.send("I'm looking for image");
+        message.channel.send("I'm looking for image");
         function sendMinion(url){
             var randomIndex = (Math.floor(Math.random() * 8));
         }
 
         if (message.content == "dab"){
-            message.channel.send(" ", { files: ["./resources/memes/dab2.png"] });
+            message.channel.send(" ", { files: ["./core/imageResponse/resources/memes/dab2.png"] });
         }
         else if (message.content == "our lord and savior"){
-            message.channel.send(" ", { files: ["./resources/memes/lord.png"] });
+            message.channel.send(" ", { files: ["./core/imageResponse/resources/memes/lord.png"] });
         }
         else if (message.content == "kaj"){
-            message.channel.send(" ", { files: ["./resources/memes/tenor.gif"] });
+            message.channel.send(" ", { files: ["./core/imageResponse/resources/memes/tenor.gif"] });
         }
         else if (message.content == "epic"){
-            message.channel.send("Le epic win", { files: ["./resources/memes/based.gif"] });
+            message.channel.send("Le epic win", { files: ["./core/imageResponse/resources/memes/epic.gif"] });
         }
 
 
