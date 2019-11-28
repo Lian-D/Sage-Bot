e75e0a2b5a710af1c2bf9d2fd28d8f329970f5cf
Lian <Lian.Duan@alumni.ubc.ca>
2019-11-27 22:54:30 -0800

    Update stockFetcher.js

--- core/stocks/stockFetcher.js
+++ core/stocks/stockFetcher.js
@@ -10,8 +10,10 @@ module.exports = {
                     message.channel.send("couldn't get stock: " + error);
               } else {
                     // message.channel.send(JSON.stringify(snapshot));
-                    message.channel.send(snapshot.price.longName
-                      + "\n**Post Market Price:** $"+snapshot.price.regularMarketPrice);
+                    message.channel.send("**"+snapshot.price.longName+"** - "+snapshot.price.symbol
+                      + "\n**Current Market Price:** $"+snapshot.price.regularMarketPrice+" "+snapshot.summaryDetai.currency
+                      + "\n**Fifty Day Avg:** $"+snapshot.summaryDetai.fiftyDayAverage+" "+snapshot.summaryDetai.currency
+                      + "\n**Two Hundred Day Avg:** $"+snapshot.summaryDetai.twoHundredDayAverage+" "+snapshot.summaryDetai.currency);
               }
           });
     }
