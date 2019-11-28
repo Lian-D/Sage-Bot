02f21d2167307a6363a47f0261773e78deee2493
Lian <Lian.Duan@alumni.ubc.ca>
2019-11-27 20:15:29 -0800

    something broke

--- core/responsesHandler.js
+++ core/responsesHandler.js
@@ -5,7 +5,7 @@ module.exports = {
         var imageResponse = require('./imageResponse/imageResponse.js');
         var stringResponse = require('./commands/stringResponse.js');
         var stockResponse = require('./stocks/stockFetcher.js');
-        var gamble = require('./gambling/roll.js');
+        // var gamble = require('./gambling/roll.js');
         var wikiResponse = require('./wikipedia/wikipedia.js');
 
         if (garfResponse.keywords.includes(message.content.toLowerCase()) || message.content.toLowerCase().includes("garf")){
@@ -21,9 +21,9 @@ module.exports = {
             var suffix = message.content.replace('stock ','');
             stockResponse.response(message,suffix);
         }
-        else if (gamble.keywords.includes(message.content.toLowerCase())  || message.content.toLowerCase().includes("roll")){
-            gamble.response(message);
-        }
+        // else if (gamble.keywords.includes(message.content.toLowerCase())  || message.content.toLowerCase().includes("roll")){
+        //     gamble.response(message);
+        // }
         
         // else if (message.content.toLowerCase().includes(wikiResponse.keywords[0]) || message.content.toLowerCase().includes(wikiResponse.keywords[1])){
         //     var query = message.content.replace (wikiResponse.keywords[0],"");
