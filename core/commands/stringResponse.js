cd31c131a4144872a71a4ca0f7acced16165c572
Chris Wang <33915928+TheDragQueen@users.noreply.github.com>
2020-02-22 23:16:23 -0800

    fuck you

--- core/commands/stringResponse.js
+++ core/commands/stringResponse.js
@@ -30,5 +30,8 @@ module.exports = {
             message.channel.send("\n⠀⠀⠀⣶⣿⣶\n⠀⠀⠀⣿⣿⣿⣀\n⠀⣀⣿⣿⣿⣿⣿⣿\n⣶⣿⠛⣭⣿⣿⣿⣿\n⠛⠛⠛⣿⣿⣿⣿⠿\n⠀⠀⠀⠀⣿⣿⣿\n⠀⠀⣀⣭⣿⣿⣿⣿⣀\n⠀⠤⣿⣿⣿⣿⣿⣿⠉\n⠀⣿⣿⣿⣿⣿⣿⠉\n⣿⣿⣿⣿⣿⣿\n⣿⣿⣶⣿⣿\n⠉⠛⣿⣿⣶⣤\n⠀⠀⠉⠿⣿⣿⣤\n⠀⠀⣀⣤⣿⣿⣿\n⠀⠒⠿⠛⠉⠿⣿\n⠀⠀⠀⠀⠀⣀⣿⣿\n⠀⠀⠀⠀⣶⠿⠿⠛");
         // DONT TAKE THIS FROM ME HE'S SPECIAL TO ME
         }
+        else if (stringReponse.keywords.includes(" ")){
+			message.channel.send("https://media.discordapp.net/attachments/648400331583258631/680164093885415438/Bing_Boing_Bing_Bing_Boing_Bing_Bing-1-1.mp4");
+        }
     }
 };
