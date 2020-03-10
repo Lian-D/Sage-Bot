4336ae13c1681714505b659c7cf69a922b883406
Chris Wang <33915928+TheDragQueen@users.noreply.github.com>
2020-03-09 21:44:44 -0700

    Update stringResponse.js

--- core/commands/stringResponse.js
+++ core/commands/stringResponse.js
@@ -3,7 +3,7 @@
 * @returns a message
 */
 module.exports = {
-    keywords: ["cs", "keikaku", "benis", "borf", "fortnite", "bong"],
+    keywords: ["cs", "keikaku", "benis", "borf", "fortnite", "bing boing"],
     response: function(message){
         if (message.content.toLowerCase().includes("cs")){
             message.channel.send("cs?");
@@ -30,7 +30,7 @@ module.exports = {
             message.channel.send("\n⠀⠀⠀⣶⣿⣶\n⠀⠀⠀⣿⣿⣿⣀\n⠀⣀⣿⣿⣿⣿⣿⣿\n⣶⣿⠛⣭⣿⣿⣿⣿\n⠛⠛⠛⣿⣿⣿⣿⠿\n⠀⠀⠀⠀⣿⣿⣿\n⠀⠀⣀⣭⣿⣿⣿⣿⣀\n⠀⠤⣿⣿⣿⣿⣿⣿⠉\n⠀⣿⣿⣿⣿⣿⣿⠉\n⣿⣿⣿⣿⣿⣿\n⣿⣿⣶⣿⣿\n⠉⠛⣿⣿⣶⣤\n⠀⠀⠉⠿⣿⣿⣤\n⠀⠀⣀⣤⣿⣿⣿\n⠀⠒⠿⠛⠉⠿⣿\n⠀⠀⠀⠀⠀⣀⣿⣿\n⠀⠀⠀⠀⣶⠿⠿⠛");
         // DONT TAKE THIS FROM ME HE'S SPECIAL TO ME
         }
-        else if (message.content.toLowerCase().includes("bong")){
+        else if (message.content == "bing boing"){
 	    message.channel.send("https://media.discordapp.net/attachments/648400331583258631/680164093885415438/Bing_Boing_Bing_Bing_Boing_Bing_Bing-1-1.mp4");
         }
     }
