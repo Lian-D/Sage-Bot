081b6be8093d55591f12c6f058d22002d5a425bd
Lian-D <LianDuan64@gmail.com>
2020-05-18 21:46:37 -0700

    Update Status

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const responsesHandler = require('./core/responsesHandler.js');
 
 client.on('ready', () => {
     console.log('I am ready!');
-    client.user.setPresence({ game: { name: 'with coomers', type: 0 } });
+    client.user.setPresence({ game: { name: 'WITH QUARANTINE', type: 0 } });
 });
 
  
